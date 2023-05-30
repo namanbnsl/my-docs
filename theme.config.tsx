cccc8be6f9361ccc25a9125c5ce066ba5f22150c
coding1101 <namanbansal1101@gmail.com>
2023-05-30 15:51:47 +0530

    init

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Airblock</span>,
+  project: {
+    link: "https://github.com/airblockai/sdk",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  footer: {
+    text: "Airblock INC", // change
+  },
+};
+
+export default config;
